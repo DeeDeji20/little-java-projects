114644d244c89f07e24d50ae0427d889d7fdc0ed
Adeola Oladeji <deolaoladeji@gmail.com>
2022-02-01 20:00:28 +0100

    update

--- myListImplementation/src/com/semicolon/africa/myList/MyList.java
+++ myListImplementation/src/com/semicolon/africa/myList/MyList.java
@@ -1,5 +1,7 @@
 package com.semicolon.africa.myList;
 
+import java.util.Arrays;
+
 public class MyList {
     private int capacity ;
     private int size;
@@ -12,18 +14,24 @@ public class MyList {
 
     public void add(String item) {
 //        if (capacity <= size) capacity= capacity+1 * 2;
+
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null){
                 items[i] = item;
                 size++;
                 break;
             }
-
-            if (items[i] != null && capacity <= size){
-                items = new String[2 * capacity];
-                items[i] = item;
-                i++;
-            }
+            System.out.println(items.length);
+        }
+        if (capacity <= size){
+            String[] fomerElements = items;
+//                System.out.println(Arrays.toString(fomerElements));
+            items = new String[2 * capacity];
+//                System.out.println(items.length);
+            items = fomerElements;
+//                System.out.println(Arrays.toString(items));
+//                items[i] = item;
+//            i++;
         }
     }
 
