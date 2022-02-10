e6601595f40b06002e13906461b0d0d9c0201d1f
Adeola Oladeji <deolaoladeji@gmail.com>
2022-02-10 16:16:26 +0100

    stack implementation

--- stackImplementation/src/MyStack.java
+++ stackImplementation/src/MyStack.java
@@ -1,11 +1,18 @@
 public class MyStack {
 
     private boolean isEmpty = true;
+    private String[] myStack = new String[5];
+    private int count;
 
     public boolean isEmpty() {
+        if (count> 0) return !isEmpty;
         return isEmpty;
     }
 
-    public void push(String... element) {
+    public void push(String element) {
+        for (int i = 0; i < myStack.length; i++) {
+            myStack[i] = element;
+            count++;
+        }
     }
 }
--- stackImplementation/test/StackTest.java
+++ stackImplementation/test/StackTest.java
@@ -1,10 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.AssertFalse.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class StackTest {
