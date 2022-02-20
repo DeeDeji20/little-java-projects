a453cb4460d008e7182c4b4b5068c1f4d0509918
Adeola Oladeji <deolaoladeji@gmail.com>
2022-02-20 05:53:32 +0100

    projects

---
+++ ticTacToeImplementation/src/Player.java
@@ -0,0 +1,23 @@
+public class Player {
+    public Cell xorO;
+    private int id;
+
+    public Player(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setXorO(Cell cell) {
+        xorO = cell;
+    }
+
+    public Cell getXorO() {
+        return xorO;
+    }
+}
